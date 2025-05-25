4a3a927760259948e7966e32f0b131f5bfa0c3fe
Austin Nelson <austinn2603@gmail.com>
2025-05-25 14:20:20 +0000

    Active user decorator

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -13,6 +13,7 @@ import { SignInDto } from '../dto/sign-in.dto';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigType } from '@nestjs/config';
 import jwtConfig from '../config/jwt.config';
+import { ActiveUserData } from '../types/active-user-data.type';
 
 @Injectable()
 export class AuthenticationService {
@@ -54,7 +55,7 @@ export class AuthenticationService {
       {
         sub: user.id,
         email: user.email,
-      },
+      } as ActiveUserData,
       {
         audience: this.jwtConfiguration.audience,
         issuer: this.jwtConfiguration.issuer,
